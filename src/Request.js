29c4192938101cd862f709b37d75b6cb070c46f3
MananPoojara <billybutcher215@gmail.com>
2024-01-26 08:57:34 +0530

    Brief description of the changes made

--- src/Request.js
+++ src/Request.js
@@ -1,12 +1,12 @@
-const requests ={
-    requestMovie : `https://api.themoviedb.org/3/movie/popular?api_key=fdaf25ad95d03eb479352313a1278001&language=en-US&page=1`,
+const requests = {
+    requestMovie: `https://api.themoviedb.org/3/movie/popular?api_key=(your api key)&language=en-US&page=1`,
 
-    requestTrending : `https://api.themoviedb.org/3/trending/tv/day?api_key=fdaf25ad95d03eb479352313a1278001&language=en-US&page=5`,
+    requestTrending: `https://api.themoviedb.org/3/trending/tv/day?api_key=(your api key)&language=en-US&page=5`,
 
-    requestTV : `https://api.themoviedb.org/3/trending/tv/day?api_key=fdaf25ad95d03eb479352313a1278001&language=en-US&page=2`,
-    PopularPeople : `https://api.themoviedb.org/3/trending/tv/week?api_key=fdaf25ad95d03eb479352313a1278001&language=en-US&page=7`,
+    requestTV: `https://api.themoviedb.org/3/trending/tv/day?api_key=(your api key)&language=en-US&page=2`,
+    PopularPeople: `https://api.themoviedb.org/3/trending/tv/week?api_key=(your api key)&language=en-US&page=7`,
 
-    UpComing : `https://api.themoviedb.org/3/trending/tv/day?api_key=fdaf25ad95d03eb479352313a1278001&language=en-US&page=4`
+    UpComing: `https://api.themoviedb.org/3/trending/tv/day?api_key=(your api key)&language=en-US&page=4`
 }
 
 export default requests;
